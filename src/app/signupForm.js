55539e9025148af6ead84e749f9e4877d5de0809
CarolPe03 <anacarolpc11@gmail.com>
2023-04-11 12:27:03 -0600

    Cambios en rutas de sensores

--- src/app/signupForm.js
+++ src/app/signupForm.js
@@ -1,6 +1,7 @@
 import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js"
 import { auth } from "./firebase.js";
 import { showMessage } from "./showMessage.js";
+import { loginCheck } from "./loginCheck.js";
 
 const signUpForm = document.querySelector("#signup-form");
 
@@ -13,6 +14,9 @@ signUpForm.addEventListener("submit", async (e) => {
     const userCredential = await createUserWithEmailAndPassword(auth, email, password)
     console.log(userCredential)
 
+    // Send email verification
+    await userCredential.user.sendEmailVerification();
+
     // Close the signup modal
     const signupModal = document.querySelector('#signupModal');
     const modal = bootstrap.Modal.getInstance(signupModal);
@@ -21,19 +25,25 @@ signUpForm.addEventListener("submit", async (e) => {
     // reset the form
     signUpForm.reset();
 
-    // show welcome message
-    // showMessage("Bienvenido a PlantLink " + userCredentials.user.email);
+    // show message to verify email
+    showMessage("Por favor, verifica tu dirección de correo electrónico antes de iniciar sesión");
+
+    // Update UI based on user's new authentication state
+    loginCheck(userCredential.user);
 
   } catch (error) {
     if (error.code === 'auth/email-already-in-use') {
-      showMessage("Este correo ya esta registrado", "error")
+      showMessage("Este correo ya está registrado", "error")
     } else if (error.code === 'auth/invalid-email') {
       showMessage("Correo incorrecto", "error")
     } else if (error.code === 'auth/weak-password') {
       showMessage("Contraseña incorrecta", "error")
     } else if (error.code) {
-      showMessage("Algo salio mal", "error")
+      showMessage("Algo salió mal", "error")
     }
   }
 
 });
+
+
+
